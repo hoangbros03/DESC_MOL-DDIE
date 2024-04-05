5cbcb290dbc561ea3b44889aca6539ed7ed646e3
hoangbros03 <87892619+hoangbros03@users.noreply.github.com>
2024-04-05 23:24:38 +0700

    re-enable random sampler

--- main/run_ddie.py
+++ main/run_ddie.py
@@ -165,9 +165,9 @@ def train(args, train_dataset, model, tokenizer, desc_tokenizer):
         tb_writer = SummaryWriter()
 
     args.train_batch_size = args.per_gpu_train_batch_size * max(1, args.n_gpu)
-    # train_sampler = RandomSampler(train_dataset) if args.local_rank == -1 else DistributedSampler(train_dataset)
-    # train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size)
-    train_dataloader = DataLoader(train_dataset, batch_size=args.train_batch_size)
+    train_sampler = RandomSampler(train_dataset) if args.local_rank == -1 else DistributedSampler(train_dataset)
+    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size)
+    # train_dataloader = DataLoader(train_dataset, batch_size=args.train_batch_size)
 
     if args.max_steps > 0:
         t_total = args.max_steps
